50008719ce3ab307291e3c96386ca2ae595278f7
ParthCodes200710 <parthcodes2007@gmail.com>
2021-07-16 09:00:23 +0530

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-import 'package:english_words/english_words.dart';
+import 'package:first_app/HomePage.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'bing boom bada boomr',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      home :HomePage(),
     );
   }
 }
